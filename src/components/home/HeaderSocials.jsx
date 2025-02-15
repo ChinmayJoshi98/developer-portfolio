4fa0206a8ca6a3192421cc7315c8fc712fbcc174
ChinmayJoshi98 <joshichinmay94@gmail.com>
2025-02-15 00:24:06 -0600

    font awesome icons added

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -2,46 +2,65 @@
 import React from "react";
 
 const HeaderSocials = () => {
+  const iconSizeClass = "fa-2x"; // enlarges icons using Font Awesome class
   return (
-    <div className="home__socials">
+    <div
+      className="home__socials"
+      style={{ display: "flex", justifyContent: "center", gap: "1rem" }}
+    >
       <a
-        href="https://www.instagram.com/"
+        href="https://www.linkedin.com/in/chinmay-joshi-7b6a911b4"
         className="home__social-link"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <i class="fa-brands fa-instagram"></i>
+        <i className={`fab fa-linkedin ${iconSizeClass}`}></i>
       </a>
 
       <a
-        href="https://www.twitter.com/"
+        href="https://github.com/ChinmayJoshi98"
         className="home__social-link"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <i class="fa-brands fa-twitter"></i>
+        <i className={`fab fa-github ${iconSizeClass}`}></i>
       </a>
 
       <a
-        href="https://www.behance.com/"
+        href="mailto:joshichinmay94@gmail.com"
         className="home__social-link"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <i class="fa-brands fa-behance"></i>
+        <i className={`fas fa-envelope ${iconSizeClass}`}></i>
       </a>
 
       <a
-        href="https://www.dribble.com/"
+        href="https://www.instagram.com/_dopaminous_/"
         className="home__social-link"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <i class="fa-brands fa-dribbble"></i>
+        <i className={`fab fa-instagram ${iconSizeClass}`}></i>
       </a>
 
       <a
-        href="https://www.pinterest.com/"
+        href="https://www.youtube.com/@Dopaminous"
         className="home__social-link"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <i class="fa-brands fa-pinterest"></i>
+        <i className={`fab fa-youtube ${iconSizeClass}`}></i>
+      </a>
+
+      <a
+        href="https://www.chess.com/member/dopaminous"
+        className="home__social-link"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Play a game of chess with me?"
+      >
+        <i className={`fas fa-chess ${iconSizeClass}`}></i>
       </a>
     </div>
   );
