66700957f6345a88aaaa002bd8e695adcee7ce9c
ChinmayJoshi98 <joshichinmay94@gmail.com>
2025-02-15 14:21:47 -0600

    logo and other stuff added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 //src/components/home/Home.jsx
 import React from "react";
 import "./home.css";
-import Me from "../../assets/avatar-1.svg";
+import Me from "../../assets/meAvatar.png";
 import HeaderSocials from "./HeaderSocials";
 import ScrollDown from "./ScrollDown";
 import Shapes from "./Shapes";
@@ -10,9 +10,9 @@ const Home = () => {
   return (
     <section className="home container" id="home">
       <div className="intro">
-        <img src={Me} alt="" className="home__img" />
-        <h1 className="home__name">Bolby Doe</h1>
-        <span className="home__education">I'm a Front-End developer</span>
+        <img src={Me} alt="" className="home__img" style={{maxWidth:'300px'}} />
+        <h1 className="home__name">Chinmay Joshi</h1>
+        <span className="home__education">I'm a Full Stack Developer and a Certified Mulesoft Developer, I love cats!</span>
 
         <HeaderSocials />
 
